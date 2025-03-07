35a8e151436627a8319b778ec3b05881f5fa754e
hyejin <yhjyhw1004@naver.com>
2025-03-07 16:12:23 +0900

    20250307

---
+++ 20250307/20396. 돌 뒤집기 게임 1.py
@@ -0,0 +1,17 @@
+T = int(input())
+
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    arr = list(map(int, input().split()))
+
+    for _ in range(M):
+        i, j = map(int, input().split())
+
+        # i부터 j까지 반복
+        for k in range(i-1, i+j-1):
+            # 범위 벗어나지 않게 k값이 배열 길이보다 작으면
+            if k < len(arr):
+                # k값을 인덱스 i번째 돌색으로 변경
+                arr[k] = arr[i-1]
+
+    print(f"#{tc}", *arr)
