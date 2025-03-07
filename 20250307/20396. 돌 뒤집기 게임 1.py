58271fc3452b5ce9e514ed2e649ecd585f3ebabb
hyejin <yhjyhw1004@naver.com>
2025-03-07 17:29:29 +0900

    20250307

--- 20250307/20396. 돌 뒤집기 게임 1.py
+++ 20250307/20396. 돌 뒤집기 게임 1.py
@@ -1,6 +1,7 @@
 T = int(input())
 
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
+    # N: 돌의 수, M: 뒤집기 횟수
     N, M = map(int, input().split())
     arr = list(map(int, input().split()))
 
@@ -8,10 +9,10 @@ for tc in range(1, T+1):
         i, j = map(int, input().split())
 
         # i부터 j까지 반복
-        for k in range(i-1, i+j-1):
+        for k in range(i - 1, i + j - 1):
             # 범위 벗어나지 않게 k값이 배열 길이보다 작으면
             if k < len(arr):
                 # k값을 인덱스 i번째 돌색으로 변경
-                arr[k] = arr[i-1]
+                arr[k] = arr[i - 1]
 
     print(f"#{tc}", *arr)
--- 20250307/20397. 돌 뒤집기 게임 2.py
+++ 20250307/20397. 돌 뒤집기 게임 2.py
@@ -1,8 +1,7 @@
 T = int(input())
 
-for tc in range(1, T+1):
-    # N: 돌의 수
-    # M: 뒤집기 횟수
+for tc in range(1, T + 1):
+    # N: 돌의 수, M: 뒤집기 횟수
     N, M = map(int, input().split())
     arr = list(map(int, input().split()))
 
@@ -10,13 +9,13 @@ for tc in range(1, T+1):
         i, j = map(int, input().split())
 
         # i번째 돌부터 j개의 돌을 비교하면서 뒤집기
-        for x in range(1, j+1):
+        for x in range(1, j + 1):
             # 값들이 0과 N 사이에 존재해야한다.
-            if 0 <= (i-1-x) < N and 0 <= (i-1+x) < N:
+            if 0 <= (i - 1 - x) < N and 0 <= (i - 1 + x) < N:
                 # 대칭되는 둘이 같은지 확인
-                if arr[i-1-x] == arr[i-1+x]:
+                if arr[i - 1 - x] == arr[i - 1 + x]:
                     # 1이면 0으로, 0이면 1로 뒤집기
-                    arr[i-1-x] = 1 - arr[i-1-x]
-                    arr[i-1+x] = 1 - arr[i-1+x]
+                    arr[i - 1 - x] = 1 - arr[i - 1 - x]
+                    arr[i - 1 + x] = 1 - arr[i - 1 + x]
 
     print(f"#{tc}", *arr)
