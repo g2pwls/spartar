a2785ec99245a254299cb73332d870b97c791116
hyejin <yhjyhw1004@naver.com>
2025-03-14 14:34:11 +0900

    20250311

--- 20250311/10760. 우주선착륙2.py
+++ 20250311/10760. 우주선착륙2.py
@@ -4,7 +4,7 @@ T = int(input())
 dr = [-1, 1, 0, 0, -1, 1, -1, 1]
 dc = [0, 0, -1, 1, 1, 1, -1, -1]
 
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     N, M = map(int, input().split())
     arr = [list(map(int, input().split())) for _ in range(N)]
     # 최종 예비 후보지 결정 난 곳 개수
